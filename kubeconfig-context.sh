e3ae8997af8abc50f4c0f95429c27d0c6b664558
Simon Godik <simon@godik.com>
2025-12-28 20:09:06 -0800

    remove duplicate blanks in get-contexts output

--- kubeconfig-context.sh
+++ kubeconfig-context.sh
@@ -8,7 +8,7 @@ function kubeconfig_use_context() {
    oc config use-context $context; rc=$?
    if (( rc > 0 )); then echo rc=$rc, context $context not found, cluster key $cluster; exit 1; fi
 
-   context_cluster_name=`oc config get-contexts | grep "*" | cut -d' ' -f59`; rc=$?
+   context_cluster_name=`oc config get-contexts | grep "*" | tr -s ' ' | cut -d' ' -f3`; rc=$?
    if (( rc > 0 )); then echo failed to get cluster name from current context $context; exit 1; fi
 
    echo current context: $context, cluster name: $context_cluster_name
